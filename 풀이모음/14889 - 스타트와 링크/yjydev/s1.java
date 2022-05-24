1110adbbe23d3191cb85add694042cd502e8ae6b
jyj <55572165+yjydev@users.noreply.github.com>
2022-05-24 15:55:59 +0900

    파일 삭제

--- 풀이모음/14889 - 스타트와 링크/yjydev/s1.java
+++
@@ -1,8 +0,0 @@
-def solution(nums):
-    max_pos = len(nums)//2
-    # 연구실에 있는 폰켓몬의 종류가 가져갈수 있는 수보다 작다면, 
-    if len(set(nums)) < max_pos:
-        return len(set(nums))
-    # 연구실에 있는 폰켓몬의 종류가 가져갈 수 있는 수보다 많다면, 어차피 n//2 마리만 가져갈 수 있으므로
-    else:
-        return max_pos
